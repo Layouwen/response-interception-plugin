152daccd6e03e2052a8a1ed04faac27de80ef630
Avan <layouwen@gmail.com>
2022-03-10 09:44:52 +0800

    refactor: refactor code

--- main.js
+++ main.js
@@ -1,35 +1,41 @@
-const origin = XMLHttpRequest.prototype.open
+startInterception()
 
-XMLHttpRequest.prototype.open = function (...args) {
-  this.responseMiddle = function (res) {
-    const _responseMiddle = window.responseMiddle
-    let result = _responseMiddle?.(res, this)
-    return result ? result : res
+function startInterception() {
+  const origin = XMLHttpRequest.prototype.open
+
+  XMLHttpRequest.prototype.open = function (...args) {
+    this.responseMiddle = function (res) {
+      const _responseMiddle = window.responseMiddle
+      let result = _responseMiddle?.(res, this)
+      return result ? result : res
+    }
+    return origin.apply(this, args)
   }
-  return origin.apply(this, args)
+
+  const keys = ['responseText', 'response']
+
+  keys.forEach(key => {
+    const accessor = Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, key)
+    Object.defineProperty(XMLHttpRequest.prototype, key, {
+      get: responseGetHandle(accessor),
+      set: responseSetHandle(accessor),
+      configurable: true,
+    })
+  })
 }
 
-const responseGetHandle = accessor =>
-  function () {
+function responseGetHandle(accessor) {
+  return function () {
     let response = accessor.get.call(this)
     if (this.responseMiddle) {
       response = this.responseMiddle(response)
     }
     return response
   }
+}
 
-const responseSetHandle = accessor =>
-  function (str) {
+function responseSetHandle(accessor) {
+  return function (str) {
     return accessor.set.call(this, str)
   }
-
-const keys = ['responseText', 'response']
-
-keys.forEach(key => {
-  const accessor = Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, key)
-  Object.defineProperty(XMLHttpRequest.prototype, key, {
-    get: responseGetHandle(accessor),
-    set: responseSetHandle(accessor),
-    configurable: true,
-  })
-})
+}
